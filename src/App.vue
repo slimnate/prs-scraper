b8b5a8535a94dfa0873c736129702377e6c65eb5
Slim Nate <slimnatekc@gmail.com>
2020-03-08 00:48:57 -0600

    implement Quasar ui framework, and remove Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar v-ripple>
+        <q-toolbar-title>
+          <q-avatar>
+            <q-icon name="album" size="lg" />
+          </q-avatar>
+          Music Content Libraries
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <library-browser />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar v-ripple>
+        <div>
+          <q-toolbar-title>
+            prs-scraper
+          </q-toolbar-title>
+        </div>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
 </template>
 
 <script>
-import libraries from "../data/libraries.json";
+import LibraryBrowser from "./components/LibraryBrowser.vue";
 
 export default {
-  data: () => {
-    return {
-      libraries
-    };
+  name: "LayoutDefault",
+
+  components: {
+    LibraryBrowser
   },
-  computed: {},
-  methods: {}
+
+  data() {
+    return {};
+  }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
---
+++ src/components/LibraryBrowser.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    
+  </q-page>
+</template>
+
+<style></style>
+
+<script>
+import libraries from '../../data/libraries.json'
+
+export default {
+  name: "LibraryBrowser",
+  data() {
+      return {
+          libraries
+      }
+  }
+};
+</script>

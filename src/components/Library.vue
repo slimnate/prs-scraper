242e4dfd4abc3091e1916ccba709b6a162487b67
Slim Nate <slimnatekc@gmail.com>
2020-03-30 20:25:41 -0500

    Add tabbed controls to each Library for description, notes, and tags components

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -5,6 +5,8 @@
     @click="click"
     dense
   >
+    <!-- expansion header -->
+    <!-- add favorites star somewhere in header. -->
     <template v-slot:header>
       <q-item-section avatar>
         <q-avatar icon="contact_mail" />
@@ -22,8 +24,44 @@
         <link-panel :links="library.links" />
       </q-item-section>
     </template>
+
+    <!-- expansion body -->
     <q-card>
-      <q-card-section v-html="library.description" class="description"></q-card-section>
+      <q-tabs v-model="tab" dense>
+        <q-tab name="description" label="Description"></q-tab>
+        <q-tab name="notes" label="Notes"></q-tab>
+        <q-tab name="tags" label="Tags"></q-tab>
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <!-- description tab -->
+        <q-tab-panel name="description">
+          <div class="text-h6">Description</div>
+
+          <div
+            class="text-body2 description"
+            v-html="library.description"
+          ></div>
+        </q-tab-panel>
+
+        <!-- notes tab-->
+        <q-tab-panel name="notes">
+          <div class="text-h6">Notes</div>
+
+          <!-- todo implement note editor -->
+        </q-tab-panel>
+
+        <!-- tags tab -->
+        <q-tab-panel name="tags">
+          <div class="text-h6">Tags</div>
+
+          <!-- todo implement tag editor -->
+        </q-tab-panel>
+      </q-tab-panels>
+
+      <!-- create tag editing components. -->
     </q-card>
   </q-expansion-item>
 </template>
@@ -36,7 +74,7 @@
 }
 
 .description {
-    margin-left: 40px;
+  margin-left: 40px;
 }
 </style>
 
@@ -57,7 +95,8 @@ export default {
 
   data() {
     return {
-      shortDescription: ""
+      shortDescription: "",
+      tab: "description"
     };
   },
 
